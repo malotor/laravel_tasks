527d6274c852893915b36c028e018be485796776
Manel <malotor@gmail.com>
2015-05-29 22:42:04 +0200

    Initial commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,99 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+use Session;
+
+class TasksController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$tasks = Task::all();
+ 
+    	return view('tasks.index')->withTasks($tasks);
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return view('tasks.create');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+		    'title' => 'required',
+		    'description' => 'required'
+		]);
+		//dd($request->all());
+		$input = $request->all();
+    	Task::create($input);
+    	Session::flash('flash_message', 'Task successfully added!');
+ 
+    	return redirect()->back();
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
